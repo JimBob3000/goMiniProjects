ae424e7aebdda088989795bc7d0373bfdd035b1f
JimBob3000 <jpelliott599@hotmail.co.uk>
2022-09-03 16:17:14 +0100

    Adding linearSearch

---
+++ linearSearch/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{6, 14, 11, 39, 5, 7, 28, 29, 40}
+	targetNumber := 28
+
+	found, guessCount := linearSearch(numbers, targetNumber)
+
+	if found {
+		fmt.Printf("Target number %v found in %v guessses.\n", targetNumber, guessCount)
+	} else {
+		fmt.Printf("Target number %v not in slice.\n", targetNumber)
+	}
+}
+
+func linearSearch(numbers []int, targetNumber int) (bool, int) {
+	for i := 0; i < len(numbers); i++ {
+		if numbers[i] == targetNumber {
+			return true, i
+		}
+	}
+
+	return false, len(numbers)
+}
