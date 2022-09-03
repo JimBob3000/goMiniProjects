cfaf03b7637510aee27936a78a4eb22684508512
JimBob3000 <jpelliott599@hotmail.co.uk>
2022-09-03 18:19:58 +0100

    Updating jump search to handle not finding target

--- jumpSearch/main.go
+++ jumpSearch/main.go
@@ -3,36 +3,47 @@ package main
 import "fmt"
 
 func main() {
-	targetNumber := 32
 	numbers := []int{1, 1, 1, 2, 5, 7, 8, 11, 11, 15, 19, 27, 29, 30, 31, 32, 33, 38, 40, 45, 46, 50}
-	jumpSearch(numbers, targetNumber)
+	targetNumber := 33
+
+	found, guessCount := jumpSearch(numbers, targetNumber)
+
+	if found {
+		fmt.Printf("Target number %v found in %v guessses.\n", targetNumber, guessCount)
+	} else {
+		fmt.Printf("Target number %v not in slice, searched %v times.\n", targetNumber, guessCount)
+	}
 }
 
-func jumpSearch(numbers []int, targetNumber int) {
-	guessCount := 0
+func jumpSearch(numbers []int, targetNumber int) (bool, int) {
+	guessCount := 1
 	currentIndex := 0
+	numberLength := len(numbers)
+	jump := 4
 
-	for i := 0; i < len(numbers)-1; i += 4 {
-		fmt.Printf("Jumping to index %v and guessing %v...\n", i, numbers[i])
-
-		if numbers[i] >= targetNumber {
-			fmt.Printf("Jump too large, reverting to index %v and moving to incremental guesses\n", currentIndex)
+	for {
+		if currentIndex > numberLength {
+			currentIndex = currentIndex - jump
 			break
 		}
 
-		currentIndex = i
+		if numbers[currentIndex] > targetNumber {
+			currentIndex = currentIndex - jump
+			break
+		}
+
+		currentIndex = currentIndex + jump
 		guessCount++
 	}
 
-	for currentIndex < len(numbers)-1 {
-		fmt.Printf("Incrementing to index %v and guessing %v...\n", currentIndex, numbers[currentIndex])
-
+	for currentIndex < numberLength {
 		if numbers[currentIndex] == targetNumber {
-			fmt.Printf("Target number %v found in %v guesses\n", numbers[currentIndex], guessCount)
-			break
+			return true, guessCount
 		}
 
 		currentIndex++
 		guessCount++
 	}
+
+	return false, guessCount
 }
