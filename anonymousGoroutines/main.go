1ba54a7d30a09b223017f61c869c48335be7e0df
JimBob3000 <jpelliott599@hotmail.co.uk>
2022-10-06 20:48:35 +0100

    Adding anonymousGoroutines

---
+++ anonymousGoroutines/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("App started")
+
+	go func(name string) {
+		fmt.Println("Welcome,", name)
+	}("James")
+
+	// Without a sleep, the main func finshes execution before the goroutine.
+	// This means the app closes and we would not see the goroutine output.
+	// We need to use wait groups to keep concurrency, not sleeps!
+	time.Sleep(100 * time.Millisecond)
+
+	fmt.Println("App finished - goroutine may not have finished!")
+}
